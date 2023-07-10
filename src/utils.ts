ce1179b523ca22697f8985918230f870a0c5f9e6
SErAphLi <seraphlivery@gmail.com>
2023-07-10 14:51:32 +0800

    perf: display two decimal places

--- src/utils.ts
+++ src/utils.ts
@@ -21,7 +21,11 @@ export function formatOOMWarning(systemInfo: SystemInfo) {
 }
 
 export function convertBytesToHumanReadable(bytes: number) {
+	// Display two decimal places
 	const sizes = ['B', 'KB', 'MB', 'GB', 'TB'];
+	if (bytes === 0) {
+		return '0 B';
+	}
 	const i = Math.floor(Math.log(bytes) / Math.log(1024));
-	return Math.round(bytes / Math.pow(1024, i)) + ' ' + sizes[i];
+	return `${parseFloat((bytes / Math.pow(1024, i)).toFixed(2))} ${sizes[i]}`;
 }
